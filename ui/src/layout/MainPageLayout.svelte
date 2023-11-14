05028ab6dc6bde55707ee595dbb232fa20b51124
Uptaker <markus.tammeoja@gmail.com>
2023-11-14 02:04:59 +0200

    add a super sneaky Github link that nobody shall ever click on, never-ever ever

--- ui/src/layout/MainPageLayout.svelte
+++ ui/src/layout/MainPageLayout.svelte
@@ -9,6 +9,7 @@
 </svelte:head>
 
 <div class="min-h-screen bg-gradient-to-tl from-blue-200 via-slate-200 to-white">
+    <a class="text-slate-500 text-xs" href="https://github.com/Uptaker/higherlowergame" target="_blank">GitHub</a>
     <div class="min-w-[360px] max-w-4xl mx-auto px-4">
         <slot/>
     </div>
