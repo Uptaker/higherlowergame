37dc259a9055e7b0e7275116f18dbba3f8f1f71c
Uptaker <markus.tammeoja@gmail.com>
2023-11-14 23:42:33 +0200

    fix question movie order

--- ui/src/humanReadableUtils.ts
+++ ui/src/humanReadableUtils.ts
@@ -8,8 +8,8 @@ export const humanReadableCategories = {
 }
 
 export const humanReadableGameCategoryQuestions = (currentMovie: Movie, nextMovie: Movie) => ({
-    VOTE_AVERAGE: `Does <i><i>${currentMovie.title}</i></i> have a higher <b>vote average</b> than <i>${nextMovie.title}</i>?`,
-    POPULARITY: `Is <i>${currentMovie.title}</i> more <b>popular</b> than <i>${nextMovie.title}</i>?`,
-    RUNTIME: `Does <i>${currentMovie.title}</i> have a longer <b>runtime</b> than <i>${nextMovie.title}</i>?`,
-    REVENUE: `Did <i>${currentMovie.title}</i> gross more <b>revenue</b> than <i>${nextMovie.title}</i>?`
+    VOTE_AVERAGE: `Does <i>${nextMovie.title}</i> have a higher <b>vote average</b> than <i>${currentMovie.title}</i>?`,
+    POPULARITY: `Is <i>${nextMovie.title}</i> more <b>popular</b> than <i>${currentMovie.title}</i>?`,
+    RUNTIME: `Does <i>${nextMovie.title}</i> have a longer <b>runtime</b> than <i>${currentMovie.title}</i>?`,
+    REVENUE: `Did <i>${nextMovie.title}</i> gross more <b>revenue</b> than <i>${currentMovie.title}</i>?`
 })
